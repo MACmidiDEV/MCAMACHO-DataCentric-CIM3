9251eaa03c21c4c26c432b7e2b50452b86c50208
MACmidiDEV <35860452+MACmidiDEV@users.noreply.github.com>
2020-12-09 22:25:09 -0500

    DCM3 | App Build | FLASK- Structure Files
    flask will handle all links with appRoute and display the injected html via the render_template function.  I have added a link to the video learning web page where I will integrate that service. The /landing route was created to hold the homepage for the site.

--- app.py
+++ app.py
@@ -7,9 +7,14 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 @app.route('/')                   
+
 @app.route('/landing') 
 def landing():
     return render_template("home.html") 
+
+@app.route('/liveLearn')
+def liveLearn():
+    return render_template("liveLearn.html")  
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
